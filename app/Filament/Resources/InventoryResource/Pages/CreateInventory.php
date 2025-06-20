bd26a2edac4e3b41742d6b489f68a550f063d6e6
presidenwashil <raihanpresiden2003@gmail.com>
2025-06-20 15:29:21 +0800

    refactor: validate item stock before creating inventory

--- app/Filament/Resources/InventoryResource/Pages/CreateInventory.php
+++ app/Filament/Resources/InventoryResource/Pages/CreateInventory.php
@@ -6,14 +6,30 @@ use App\Filament\Resources\InventoryResource;
 use App\Models\Item;
 use App\Models\ItemHistory;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Validation\ValidationException;
 
 class CreateInventory extends CreateRecord
 {
     protected static string $resource = InventoryResource::class;
 
-        protected function afterCreate(): void
+    protected function beforeCreate(): void
     {
+        $item = Item::find($this->data['item_id']);
+
+        if (!$item || $item->stock <= 0) {
+            Notification::make()
+                ->title('Insufficient stock')
+                ->body('The item stock is insufficient to create this inventory.')
+                ->warning()
+                ->send();
+
+            throw ValidationException::withMessages([
+                'item_id' => 'The selected item does not have sufficient stock.',
+            ]);
+        }
+
         $inventory = $this->record;
         $item = Item::find($inventory->item_id);
 
