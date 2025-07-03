9fac5e2b9be137c8f15b1ded11480f98557cef24
presidenwashil <raihanpresiden2003@gmail.com>
2025-07-03 08:33:07 +0800

    feat: refactor AppServiceProvider to modularize configuration methods

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -27,6 +30,44 @@ final class AppServiceProvider extends ServiceProvider
             DB::statement('PRAGMA foreign_keys = ON;');
         }
 
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+        $this->configureVite();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    /**
+     * Configure the application's dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the application's models.
+     */
+    private function configureModels(): void
+    {
         Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    /**
+     * Configure the application's Vite instance.
+     */
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
     }
 }
