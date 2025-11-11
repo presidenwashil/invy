425edf803b1020d632c9e9ee9bbe0b6a57272ca9
presidenwashil <raihanpresiden2003@gmail.com>
2025-11-11 12:01:40 +0800

    Remove SSR temp path configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,11 +32,9 @@ final class AppServiceProvider extends ServiceProvider
         }
 
         $view = config('view.compiled');
-        $ssr = config('ssr.node.temp_path');
-        foreach ([$view, $ssr] as $path) {
-            if (! is_dir($path)) {
-                mkdir($path, 0755, true);
-            }
+
+        if (! is_dir($view)) {
+            mkdir($view, 0755, true);
         }
 
         if (config('database.default') === 'sqlite') {
