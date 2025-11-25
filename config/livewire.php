3d79171c7543c8e1ad51e4338eb099ef157664ea
Raihan Presiden Washil <raihanpresiden2003@gmail.com>
2025-11-25 21:33:04 +0800

    Update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -67,8 +67,8 @@ return [
 
     'temporary_file_upload' => [
         'disk' => 'r2',        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'rules' => ['file', 'max:10240'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => 'livewire-tmp',   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
@@ -76,7 +76,7 @@ return [
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
-        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
+        'cleanup' => false, // Should cleanup temporary uploads older than 24 hrs...
     ],
 
     /*
