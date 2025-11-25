b32bdf2b75c13d1e5519a8e2e5b286b2a3c5a24e
Raihan Presiden Washil <raihanpresiden2003@gmail.com>
2025-11-25 21:17:03 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 use App\Http\Controllers\HandoverPdfController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/handover/{record}/pdf', HandoverPdfController::class)->name('handover.pdf');
 Route::get('/handover/pdf', App\Http\Controllers\ListHandoverPdfController::class)->name('list-handover');
+
+Route::get('/test-vercel-storage', function () {
+    dd(Storage::disk('r2')->put('test.txt', 'hello'));
+});
